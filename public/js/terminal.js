0968e6585efd9011b6631e986f61e7c663f706d7
aktaktaw <aktaktaw@gmail.com>
2020-08-28 14:29:50 +0800

    create index about portfolio page
    add terminal style css in index page

---
+++ public/js/terminal.js
@@ -0,0 +1,2 @@
+var termynal = new Termynal('#termynal');
+
---
+++ server.js
@@ -0,0 +1,28 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.use(express.static('public'))
+
+app.set('view engine','ejs')
+
+// index page 
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+// about page 
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+// skill page 
+app.get('/portfolio', function(req, res) {
+  res.render('pages/portfolio');
+});
+
+app.listen(port, () => {
+  console.log(`Portfolio app is listening at http://localhost:${port}`)
+})
